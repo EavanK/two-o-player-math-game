962eda96cd11e11d36b847ae94a33554de6fdfed
EavanK <eavan.hsk@gmail.com>
2022-01-03 13:32:13 -0800

    add play, turn_over function

--- game.rb
+++ game.rb
@@ -7,4 +7,33 @@ class Game
     @player2 = Player.new('Player 2')
     @players = [@player1, @player2]
   end
+
+  def turn_over
+    puts "\n----- NEW TURN -----"
+  end
+
+  # play game
+  def play
+    puts "\n----- GAME START -----"
+
+    question = Question.new
+
+    puts "#{@players.first.name}: what does #{question.first_num} plus #{question.second_num} equal?"
+    print "> "
+    player_answer = gets.chomp.to_i
+
+    # if a player answers wrong, lose 1 life
+    puts (
+      if player_answer == question.answer
+        "#{@players.first.name}: YES! You are correct."
+      else
+        @players.first.wrong_answer 
+        "#{@players.first.name}: Seriously? No!" 
+      end
+    )
+    puts "P1: #{@player1.lives}/3 vs P2: #{@player2.lives}/3"
+
+    @players.reverse!
+    turn_over
+  end
 end
