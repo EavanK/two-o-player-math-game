c178458ebe23738fb5414097f7aaee67878bcd68
EavanK <eavan.hsk@gmail.com>
2022-01-03 14:02:54 -0800

    modify numbers range

--- question.rb
+++ question.rb
@@ -2,8 +2,8 @@ class Question
   attr_reader :first_num, :second_num, :answer
 
   def initialize
-    @first_num = rand(1..10)
-    @second_num = rand(1...10)
+    @first_num = rand(1..20)
+    @second_num = rand(1..20)
     @answer = @first_num + @second_num
   end
 end
